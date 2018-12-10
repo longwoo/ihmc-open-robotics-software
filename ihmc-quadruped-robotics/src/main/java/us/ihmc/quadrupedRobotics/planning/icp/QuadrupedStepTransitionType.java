311309738fa25a94f52a7c1568a7529576d04746
Robert <rgriffin@ihmc.us>
2018-12-10 12:40:20 -0600

    started working on a new quadruped contact sequence calculator

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/icp/QuadrupedStepTransitionType.java
+++ ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/planning/icp/QuadrupedStepTransitionType.java
@@ -1,7 +1,5 @@
 package us.ihmc.quadrupedRobotics.planning.icp;
 
-import us.ihmc.quadrupedBasics.gait.QuadrupedTimedStep;
-
 /**
  * Defines the contact even transition type. This is useful for converting {@link QuadrupedTimedStep} to {@link QuadrupedContactPhase}.
  */
